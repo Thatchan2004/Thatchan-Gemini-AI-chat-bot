62fb800f1f1500496b89448e80f729b14b321e0a
Thatchan2004 <164884455+Thatchan2004@users.noreply.github.com>
2024-08-22 01:10:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import google.generativeai as genai
+
+
+st.title("welcome to Dr.Mental Health chat")
+
+genai.configure(api_key="AIzaSyCnGfSjBFZ42AtjIJ7IlGCM3wSGjkzh7LI")  #to get api-key https://aistudio.google.com/app/apikey
+
+text = st.text_input("enter your question")
+
+model = genai.GenerativeModel('gemini-pro')
+chat = model.start_chat(history=[])
+
+
+if st.button("Click me"):
+    response = chat.send_message(text)
+    st.write(response.text)
