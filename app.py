fbdb2e759c896c539fa3aebd2bcf4d0801fde329
Thatchan2004 <164884455+Thatchan2004@users.noreply.github.com>
2025-08-27 11:48:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import google.generativeai as genai
 
 st.title("welcome to Thatchan AI chat")
 
-genai.configure(api_key="AIzaSyCnGfSjBFZ42AtjIJ7IlGCM3wSGjkzh7LI")  #to get api-key https://aistudio.google.com/app/apikey
+genai.configure(api_key="AIzaSyB9Fnu2d36mWJ1nL50R_wQJOK93zxCd7h8")  #to get api-key https://aistudio.google.com/app/apikey
 
 text = st.text_input("enter your question")
 
@@ -15,3 +15,4 @@ chat = model.start_chat(history=[])
 if st.button("Click me"):
     response = chat.send_message(text)
     st.write(response.text)
+
